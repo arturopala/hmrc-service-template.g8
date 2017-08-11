9a0cc2e1b12f2ef69bb7b0af5b69ed2117739dec
John Revill <john.revill@digital.hmrc.gov.uk>
2017-08-11 14:09:19 +0100

    Adding unit tests for the ErrorHandler

--- src/main/g8/app/ErrorHandler.scala
+++ src/main/g8/app/ErrorHandler.scala
@@ -1,36 +1,46 @@
-import javax.inject.Singleton
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import javax.inject.{Inject, Singleton}
+
+import play.api.Configuration
+import play.api.http.HeaderNames.CACHE_CONTROL
 import play.api.http.HttpErrorHandler
-import play.api.http.Status._
-import play.api.libs.json.{Json, OFormat}
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
 import play.api.mvc.Results._
 import play.api.mvc.{RequestHeader, Result}
-import uk.gov.hmrc.play.http.{HttpException, Upstream4xxResponse, Upstream5xxResponse}
+import uk.gov.hmrc.agentepayeregistrationfrontend.views.html.error_template
 
 import scala.concurrent.Future
 
 @Singleton
-class ErrorHandler extends HttpErrorHandler {
-
-  case class ErrorResponse(statusCode: Int, message: String, xStatusCode: Option[String] = None, requested: Option[String] = None)
-
-  implicit val erFormats: OFormat[ErrorResponse] = Json.format[ErrorResponse]
+class ErrorHandler @Inject()(implicit configuration: Configuration, val messagesApi: MessagesApi) extends HttpErrorHandler with I18nSupport {
 
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
-    Future.successful(
-      Status(statusCode)(Json.toJson(ErrorResponse(statusCode, message, requested = Some(request.uri))))
-    )
+    Future successful
+      Status(statusCode)(error_template(
+        Messages(s"global.error.$statusCode.title"),
+        Messages(s"global.error.$statusCode.heading"),
+        Messages(s"global.error.$statusCode.message")))
   }
 
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
-    val errorResponse = exception match {
-      case e: HttpException => ErrorResponse(e.responseCode, e.getMessage)
-      case e: Upstream4xxResponse => ErrorResponse(e.reportAs, e.getMessage)
-      case e: Upstream5xxResponse => ErrorResponse(e.reportAs, e.getMessage)
-      case e: Throwable => ErrorResponse(INTERNAL_SERVER_ERROR, e.getMessage)
-    }
-    Future.successful(new Status(errorResponse.statusCode)(Json.toJson(errorResponse)))
+    Future successful InternalServerError(error_template(
+      Messages("global.error.500.title"),
+      Messages("global.error.500.heading"),
+      Messages("global.error.500.message"))).withHeaders(CACHE_CONTROL -> "no-cache")
   }
 }
-
-
